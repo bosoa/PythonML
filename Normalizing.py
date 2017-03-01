8880004d63fe016546df9891b69325860d2399aa
bosoa <bosoagalaxy@gmail.com>
2017-03-01 13:27:36 -0500

    master/Normalizing.py/add functions

--- Normalizing.py
+++ Normalizing.py
@@ -42,3 +42,40 @@ FEATURES = ['DE Ratio',
             'Short % of Float',
             'Shares Short (prior ']
 
+
+def Build_Data_Set():
+        data_df = pd.DataFrame.from_csv("key_stats.csv")
+
+        # data_df = data_df[:100]
+
+        X = np.array(data_df[FEATURES].values)  # .tolist())
+
+        y = (data_df["Status"]
+             .replace("underperform", 0)
+             .replace("outperform", 1)
+             .values.tolist())
+
+        X = preprocessing.scale(X)
+
+        return X, y
+
+
+def Analysis():
+
+    test_size = 1000
+    X, y = Build_Data_Set()
+    print(len(X))
+
+    clf = svm.SVC(kernel="linear", C=1.0)
+    clf.fit(X[:-test_size], y[:-test_size])
+
+    correct_count = 0
+
+    for x in range(1, test_size+1):
+        if clf.predict(X[-x])[0] == y[-x]:
+            correct_count += 1
+
+    print("Accuracy:", (correct_count/test_size) * 100.00)
+
+
+Analysis()
